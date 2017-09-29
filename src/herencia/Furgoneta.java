c53f8da24a9e3b019804369674189bba3717f517
punts hola mundo <anthonnyc10@gmail.com>
2017-09-28 20:29:20 -0600

    Uso de herencia

---
+++ src/herencia/Furgoneta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author Anthonny
+ */
+public class Furgoneta extends Vehiculo {
+    public int capasidadPersonas;
+    public boolean disponible;
+
+    public Furgoneta(String marca, String color, String matricula, double precio) {
+        super(marca, color, matricula, precio);
+    }
+
+    
+    
+}
---
+++ src/herencia/Vehiculo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author Anthonny
+ */
+public class Vehiculo {
+    public String marca;
+    public String color;
+    public String matricula;
+    public double precio;
+
+    public Vehiculo(String marca, String color, String matricula, double precio) {
+        this.marca = marca;
+        this.color = color;
+        this.matricula = matricula;
+        this.precio = precio;
+    }
+
+    
+    
+    
+}
